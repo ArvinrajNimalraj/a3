a9d1710cccf6e0e222a3221306fc542aae83478b
ArvinrajNimalraj <158398143+ArvinrajNimalraj@users.noreply.github.com>
2025-02-10 18:25:23 -0500

    Update Main.java

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -5,14 +5,59 @@ import java.io.File;
 import java.io.FileReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import java.io.IOException;
+import org.apache.commons.cli.*;
 
 public class Main {
 
-    private static final Logger logger = LogManager.getLogger();
+    private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        System.out.println("** Starting Maze Runner");
+        logger.info("** Starting Maze Runner");
+        if (cmd == numm) {
+            return;
+
+        String mazeFile = cmd.getOptionValue("i");
         try {
+            Maze maze = new Maze(mazeFile);
+            logger.info("*** Maze Loaded Successfully ***");
+
+            maze.printMaze();
+
+            logger.info("*** Computing path ***");
+            
+            MazeSolver solver = new MazeSolver(maze);
+            String path = solver.solve();
+
+            if (path.isEmpty()) {
+                logger.warn("No path found!");
+                System.out.println("PATH NOT FOUND");
+            } else {
+                System.out.println("Path: " + path);
+                logger.info("Path computed: " + path);
+            }
+        } catch (IOException e) {
+              logger.error("Error reading maze file", e):
+            }
+
+        logger.info("*** End of MazeRunner ***");
+        }
+
+
+    private static CommandLine parseArgments(String[] args) {
+        Options options = new Options();
+        options.addOption("i", "input", true, "Patch to the maze file");
+
+        CommandLineParser parser = new DefaultParser();
+        try {
+            return parser.parse(options, args);
+        } catch (ParseException e) {
+            System.err.println("Invalid arguments. Usage: -i <maze file>");
+            return null;
+        }
+    }
+
+        
             System.out.println("**** Reading the maze from file " + args[0]);
             BufferedReader reader = new BufferedReader(new FileReader(args[0]));
             String line;
