e4ad380f92a2e4a814fbac319daff2002fd4721c
ArvinrajNimalraj <158398143+ArvinrajNimalraj@users.noreply.github.com>
2025-02-11 17:27:59 -0500

    Update Maze.java

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
@@ -1,1 +1,91 @@
+pacakge ca.mcmaster.se2aa4.mazerunner;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Maze {
+  private static final Logger logger = LogManager.getLogger();
+
+  private final List<List<Boolean>> maze = new ArrayList<>();
+
+  private final Position start;
+  private final Position end;
+
+
+  public Maze(String filePath) throws Exception {
+    logger.debug("Reading the maze from file" + filePath);
+    BufferedReader reader = new BufferedReader(new FileReader(filePath));
+    String line;
+    while ((line = reader.readline()) != null) {
+      List<Boolean> newLone = new ArrayList<>();
+      for (int idx = 0; idx < line.length(); idx++) {
+        newLine.add(line.charAt(idx) == '#');
+      }
+      maze.add(newLine);
+    }
+    reader.close();
+    start = findStart();
+    end = findEnd();
+  }
+
+
+
+  private Position findStart() throws Exception {
+    for (int y = 0; y < maze.size(); y++) {
+      Position pos = new Position(0, y);
+      if (!isWall(pos)) {
+        return pos;
+      }
+    }
+    throws new Exception("Invalid maze (no start position available)");
+  } 
+
+
+  private Position findEnd() throws Exception {
+    int lastColumn = maze.get(0).size() - 1;
+    for (int y = 0; y < maze.size(); y++) {
+      Position pos = new Position(lastColumn, y);
+      if (!isWall(pos)) {
+        return pos;
+      }
+    }
+    throw new Exception("Invalid maze (no end position available)");
+  }
+
+
+  public Boolean isWall(Position pos) {
+    return maze.get(pos.y()).get(pos.x());
+  }
+
+  public Position getStart() {
+    return start;
+  }
+
+  public Position getEnd() {
+    return end
+  }
+
+  public int getSize() {
+    return this.,aze.get(0).size();
+  }
+
+  public int getSizeY() {
+    return this.maze.size();
+  }
+  
+
+
+
+
+
+
+
+
+  
+}
