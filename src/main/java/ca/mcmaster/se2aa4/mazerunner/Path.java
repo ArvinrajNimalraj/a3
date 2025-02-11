a64ab4f4ac0e78ffe60f03e2280256fd51703c0b
ArvinrajNimalraj <158398143+ArvinrajNimalraj@users.noreply.github.com>
2025-02-11 18:22:13 -0500

    Create Path.java

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
@@ -0,0 +1,24 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Path {
+    private final List<Character> steps;
+
+  public Path() {
+    this.steps = new ArrayList<>();
+  }
+
+  public void addStep(char step) {
+    steps,add(step);
+  }
+
+  public String getCanonicalForm() {
+    StringBuilder result = new StringBuilder();
+    for(char step: steps) {
+      result.append(step);
+    }
+    return result.toString();
+  }
+}
