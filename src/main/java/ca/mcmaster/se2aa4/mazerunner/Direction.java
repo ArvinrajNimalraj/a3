9be4877899a7150360d2679ab7570fa624ed75f5
ArvinrajNimalraj <158398143+ArvinrajNimalraj@users.noreply.github.com>
2025-02-11 18:31:31 -0500

    Create Direction.java

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Direction.java
@@ -0,0 +1,37 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+public enum Direction {
+  UP(0, -1), DOWN(0, 1), LEFT(-1, 0), RIGHT(1, 0);
+
+  private final int dx, dy;
+
+  Direction(int dx, int dy) {
+    this.dx = dx;
+    this.dy = dy;
+  }
+
+  public Direction turnRight() {
+    return switch (this) {
+      case UP -> RIGHT;
+      case RIGHT -> DOWN;
+      case DOWN -> LEFT;
+      case LEFT -> UP;
+    };
+  }
+  
+    public Direction turnLeft() {
+    return switch (this) {
+      case UP -> LEFT;
+      case LEFT -> DOWN;
+      case DOWN -> RIGHT;
+      case RIGHT -> UP;
+    };
+  }
+
+  public int getDx() { return dx; }
+  public int getDy() { return dy; }
+
+}
+
+
+  
