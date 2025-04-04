2b26b4f7a8c240a0a98535406fd0e7375d868895
ArvinrajNimalraj <158398143+ArvinrajNimalraj@users.noreply.github.com>
2025-04-04 00:44:37 -0400

    Update Path.java

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
@@ -6,20 +6,35 @@ import java.util.List;
 public class Path {
     private final List<Character> path = new ArrayList<>();
 
+    /**
+     * Initialize an empty Path.
+     */
     public Path() {
     }
 
-    public Path(string pathStc) {
+    /**
+     * Initialize path from a Path String.
+     *
+     * @param pathStr The Path String
+     */
+    public Path(String pathStr) {
         String expanded = expandFactorizedStringPath(pathStr);
         for (Character c : expanded.toCharArray()) {
             if (c != ' ') {
                 if (c != 'F' && c != 'L' && c != 'R') {
-                    throw new IllegalArgumentException("IOnstruction '" + c + "' is invalid. Must be 'F', 'L', or 'R'.");
+                    throw new IllegalArgumentException("Instruction '" + c + "' is invalid. Must be 'F', 'L', or 'R'.");
                 }
                 addStep(c);
             }
         }
     }
+
+    /**
+     * Expand a factorized string path into a canonical one.
+     *
+     * @param path String path
+     * @return Expanded string path
+     */
     public String expandFactorizedStringPath(String path) {
         StringBuilder expanded = new StringBuilder();
 
@@ -32,61 +47,83 @@ public class Path {
                 do {
                     count *= (int) Math.pow(10, digit++);
                     count += Character.getNumericValue(path.charAt(i++));
-                } while (Charcter.isDigit(path.charAt(i)));
+                } while (Character.isDigit(path.charAt(i)));
 
                 String step = String.valueOf(path.charAt(i)).repeat(count);
                 expanded.append(step);
             }
         }
+
         return expanded.toString();
     }
 
+    /**
+     * Get steps of Path.
+     *
+     * @return Chars of Path
+     */
     public List<Character> getPathSteps() {
         return new ArrayList<>(this.path);
     }
 
+    /**
+     * Adds a step to the path.
+     *
+     * @param step The step that needs to be added.
+     */
     public void addStep(Character step) {
         path.add(step);
     }
 
+    /**
+     * Generates the canonical form of the maze path.
+     *
+     * @return A string of the canonical form of a path.
+     */
     public String getCanonicalForm() {
         StringBuilder sb = new StringBuilder();
 
-    for (Character c : path) {
-        if (sb.isEmpty() || sb.charAt(sb.length() - 1) == c) {
-            sb.append(c);
-        } else {
-            sb.append(' ');
-            sb.append(c);
+        for (Character c : path) {
+            if (sb.isEmpty() || sb.charAt(sb.length() - 1) == c) {
+                sb.append(c);
+            } else {
+                sb.append(' ');
+                sb.append(c);
+            }
         }
-    }
-    return sb.toString();
+
+        return sb.toString();
     }
 
+    /**
+     * Generates the factorized form of the maze path.
+     *
+     * @return A string of the factorized form of a path.
+     */
     public String getFactorizedForm() {
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < path.size(); i ++) {
+        for (int i = 0; i < path.size(); i++) {
             Character current = path.get(i);
             int count = 0;
-            while(i < path.size() && current == path.get(i)) {
+            while (i < path.size() && current == path.get(i)) {
                 count++;
                 i++;
             }
-        
-            if (count ==1) {
+
+            if (count == 1) {
                 sb.append(current);
             } else {
                 sb.append(count);
                 sb.append(current);
             }
-    
+
             if (i != path.size()) {
                 sb.append(' ');
             }
             i--;
         }
-        
+
         return sb.toString();
     }
 }
