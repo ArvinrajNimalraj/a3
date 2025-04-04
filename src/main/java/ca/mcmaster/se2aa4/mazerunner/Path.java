6d70a39117e184f2d5eed749ddbffeac8cb80aae
ArvinrajNimalraj <158398143+ArvinrajNimalraj@users.noreply.github.com>
2025-04-04 01:58:10 -0400

    Create PathExpansionStrategy Interface

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
@@ -3,6 +3,10 @@ package ca.mcmaster.se2aa4.mazerunner;
 import java.util.ArrayList;
 import java.util.List;
 
+public interface PathExpansionStrategy {
+    String expand(String path);
+}
+
 public class Path {
     private final List<Character> path = new ArrayList<>();
 
