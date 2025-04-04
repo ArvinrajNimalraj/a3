5ec139b3ff05eb70f95623ca8075717487d89ccb
ArvinrajNimalraj <158398143+ArvinrajNimalraj@users.noreply.github.com>
2025-04-04 15:36:50 -0400

    Update RightHandSolver.java

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/RightHandSolver.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/RightHandSolver.java
@@ -1,3 +1,40 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class RightHandSolver implements MazeSolver {
+    private static final Logger logger = LogManager.getLogger();
+
+    @Override
+    public Path solve(Maze maze) {
+        Path path = new Path();
+        Position currentPos = maze.getStart();
+        Direction dir = Direction.RIGHT;
+
+        // Initialize commands
+        Command turnRightCommand = new TurnRightCommand();
+        Command moveForwardCommand = new MoveForwardCommand();
+        Command turnLeftCommand = new TurnLeftCommand();
+        Command turnAroundCommand = new TurnAroundCommand();
+
+        while (!currentPos.equals(maze.getEnd())) {
+            if (!maze.isWall(currentPos.move(dir.turnRight()))) {
+                turnRightCommand.execute(currentPos, dir, path, maze);
+            } else if (!maze.isWall(currentPos.move(dir))) {
+                moveForwardCommand.execute(currentPos, dir, path, maze);
+            } else if (!maze.isWall(currentPos.move(dir.turnLeft()))) {
+                turnLeftCommand.execute(currentPos, dir, path, maze);
+            } else {
+                turnAroundCommand.execute(currentPos, dir, path, maze);
+            }
+            logger.debug("Current Position: " + currentPos + "\nCurrent Path: " + path.getCanonicalForm());
+        }
+
+        return path;
+    }
+}
+
 /*
 package ca.mcmaster.se2aa4.mazerunner;
 
