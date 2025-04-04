6d457548afb15f5e698195d1f310ae2d3301db4c
ArvinrajNimalraj <158398143+ArvinrajNimalraj@users.noreply.github.com>
2025-04-04 00:40:16 -0400

    Update RightHandSolver.java

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/RightHandSolver.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/RightHandSolver.java
@@ -4,7 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class RightHandSolver implements MazeSolver {
-    private Static final Logger logger = LogManager.getLogger();
+    private static final Logger logger = LogManager.getLogger();
 
     @Override
     public Path solve(Maze maze) {
@@ -14,30 +14,32 @@ public class RightHandSolver implements MazeSolver {
         Direction dir = Direction.RIGHT;
         while (!currentPos.equals(maze.getEnd())) {
             if (!maze.isWall(currentPos.move(dir.turnRight()))) {
+                // Turn right and move forward if not a wall
                 dir = dir.turnRight();
                 path.addStep('R');
                 currentPos = currentPos.move(dir);
                 path.addStep('F');
             } else {
                 if (!maze.isWall(currentPos.move(dir))) {
+                    // Go forward if not a wall
                     currentPos = currentPos.move(dir);
                     path.addStep('F');
                 } else if (!maze.isWall(currentPos.move(dir.turnLeft()))) {
+                    // Go left if not a wall
                     dir = dir.turnLeft();
                     path.addStep('L');
-                    cuurentPos = currentPos.move(dir);
+                    currentPos = currentPos.move(dir);
                     path.addStep('F');
                 } else {
+                    // Turn around
                     dir = dir.turnRight().turnRight();
                     path.addStep('R');
                     path.addStep('R');
                 }
             }
-            logger.debug("Current Position: " currentPos.toString() + "\n Current Path: " + path.getCanonicalForm()));
+            logger.debug("Current Position: " + currentPos.toString() + "\n Current Path: " + path.getCanonicalForm());
         }
 
         return path;
     }
 }
-
-
