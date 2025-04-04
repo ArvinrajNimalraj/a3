2fc76d4b87afb57de9fe42660d70f999e646061e
ArvinrajNimalraj <158398143+ArvinrajNimalraj@users.noreply.github.com>
2025-04-03 22:48:51 -0400

    Update Path.java

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
@@ -4,25 +4,89 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Path {
-    private final List<Character> steps;
+    private final List<Character> path = new ArrayList<>();
 
-  public Path() {
-    this.steps = new ArrayList<>();
-  }
-
-  public void addStep(char step) {
-    steps.add(step);
-  }
-
-  public String getCanonicalForm() {
-    StringBuilder result = new StringBuilder();
-    for(char step : steps) {
-      result.append(step);
+    public Path() {
     }
-    return result.toString();
-  }
 
-    public boolean isEmpty() {
-        return steps.isEmpty();
+    public Path(string pathStc) {
+        String expanded = expandFactorizedStringPath(pathStr);
+        for (Character c : expanded.toCharArray()) {
+            if (c != ' ') {
+                if (c != 'F' && c != 'L' && c != 'R') {
+                    throw new IllegalArgumentException("IOnstruction '" + c + "' is invalid. Must be 'F', 'L', or 'R'.");
+                }
+                addStep(c);
+            }
+        }
+    }
+    public String expandFactorizedStringPath(String path) {
+        StringBuilder expanded = new StringBuilder();
+
+        for (int i = 0; i < path.length(); i++) {
+            if (!Character.isDigit(path.charAt(i))) {
+                expanded.append(path.charAt(i));
+            } else {
+                int count = 0;
+                int digit = 0;
+                do {
+                    count *= (int) Math.pow(10, digit++);
+                    count += Character.getNumericValue(path.charAt(i++));
+                } while (Charcter.isDigit(path.charAt(i)));
+
+                String step = String.valueOf(path.charAt(i)).repeat(count);
+                expanded.append(step);
+            }
+        }
+        return expanded.toString();
+    }
+
+    public List<Character> getPathSteps() {
+        return new ArrayList<>(this.path);
+    }
+
+    public void addStep(Character step) {
+        path.add(step);
+    }
+
+    public String getCanonicalForm() {
+        StringBuilder sb = new StringBuilder();
+
+    for (Character c : path) {
+        if (sb.isEmpty() || sb.charAt(sb.length() - 1) == c) {
+            sb.append(c);
+        } else {
+            sb.append(' ');
+            sb.append(c);
+        }
+    }
+    return sb.toString();
+    }
+
+    public String getFactorizedForm() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < path.size(); i ++) {
+            Character current = path.get(i);
+            int count = 0;
+            while(i < path.size() && current == path.get(i)) {
+                count++;
+                i++;
+            }
+        
+            if (count ==1) {
+                sb.append(current);
+            } else {
+                sb.append(count);
+                sb.append(current);
+            }
+    
+            if (i != path.size()) {
+                sb.append(' ');
+            }
+            i--;
+        }
+        
+        return sb.toString();
     }
 }
